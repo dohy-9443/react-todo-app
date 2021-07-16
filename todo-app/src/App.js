9abbfdd0fb6b4fbff91e6fef6ed521cad55fef98
dohy-9443 <dong_hyun95@hanmail.net>
2021-07-16 15:15:25 +0900

    렌더링

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import './App.css';
+import TodoTemplate from './components/TodoTemplate';
 
 const App = () => {
-  return <div>Todo App을 만들자!</div>;
+  return <TodoTemplate>Todo App을 만들자1</TodoTemplate>;
 };
 
 export default App;
