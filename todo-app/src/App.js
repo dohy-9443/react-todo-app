e308337f9c97bbcaecd4a505942c10d13939ced7
dohy-9443 <dong_hyun95@hanmail.net>
2021-07-16 15:16:42 +0900

    렌더링 후 이미지

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import TodoTemplate from './components/TodoTemplate';
 
 const App = () => {
-  return <TodoTemplate>Todo App을 만들자1</TodoTemplate>;
+  return <TodoTemplate>Todo App을 만들자!</TodoTemplate>;
 };
 
 export default App;
